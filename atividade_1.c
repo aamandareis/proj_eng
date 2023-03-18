e78768055eb2dfec3cfb0d33821bdb2e8da4bcda
aamandareis <128102250+aamandareis@users.noreply.github.com>
2023-03-18 18:17:14 -0300

    Update atividade_1.c

--- atividade_1.c
+++ atividade_1.c
@@ -2,7 +2,7 @@
 int main(){
 
 	printf("Nome: Izaac Soares Nery Junior, Matrícula: 202206840027 \n");
-	printf("Nome: ........., Matrícula: ........ \n");
+	printf("Nome: Amanda Caroline Mendes Reis, Matrícula: 202207040027 \n");
 	printf("Nome: ........., Matrícula: ........ \n");
 	printf("Nome: ........., Matrícula: ........ \n");
 	
