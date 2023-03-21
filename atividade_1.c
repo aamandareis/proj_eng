f96c609156f9b69d9cb124aea07febd8e892d17f
ZKZOV <izaacsoaresjr@gmail.com>
2023-03-20 22:46:15 -0300

    primeira contribuição

--- atividade_1.c
+++ atividade_1.c
@@ -3,7 +3,7 @@ int main(){
 
 	printf("Nome: Izaac Soares Nery Junior, Matrícula: 202206840027 \n");
 	printf("Nome: Amanda Caroline Mendes Reis, Matrícula: 202207040027 \n");
-	printf("Nome: Matheus Silva Travesso, Matrícula:  202207040038 \n");
+	printf("Nome: Matheus Silva Travassos, Matrícula:  202207040038 \n");
 	printf("Nome: João Victor Ferreira, Matrícula: 202207040028 \n");
 	
 
