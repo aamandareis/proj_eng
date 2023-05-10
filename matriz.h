370a0c5e0c0a68837252aafcb4df76e3b219b00e
aamandareis <128102250+aamandareis@users.noreply.github.com>
2023-05-10 00:05:02 -0300

    Update matriz.h

--- matriz.h
+++ matriz.h
@@ -15,32 +15,27 @@
 */
 
 typedef struct matriz Matriz;
-//armazena a quantidade de linhas, colunas e os valores dos indices da matriz;
+
 
 Matriz* novaMatriz(int lin, int col);
-//Fun��o de inicializa��o de uma matriz, recebendo o numero de linhas e colunas e retorna um ponteiro do tipo Matriz.
-//ex: mat = novaMatriz(2, 2);
+
 
 void preencherMatriz(Matriz *m);
-//Procedimento de inser��o dos valores de uma matriz, linha a linha;
+
 
 void exibirMatriz(Matriz *m);
-//Procedimento de exibi��o dos valores de uma matriz, linha a linha;
+
 
 Matriz* somarMatriz(Matriz *m1, Matriz *m2);
-//Fun��o para somar matrizes. Recebe dois ponteiros das matrizes que quer somar e retorna um ponteiro da matriz resultante.
-//ex: matSoma = somarMatriz(mat1, mat2);
+
 
 Matriz* subtrairMatriz(Matriz *m1, Matriz *m2);
-//Mesma l�gica da soma
-//ex: matSub = subtrairMatriz(mat1, mat2);
+
 
 Matriz* transposta(Matriz *m);
-//Fun��o para transpor matrizes. Recebe uma matriz e retorna sua transposta;
-//ex: mat_T = transposta(mat);
+
 
 Matriz* produtoMatriz(Matriz *m1, Matriz *m2);
-//Fun��o para multiplicar matrizes. Recebe dois ponteiros das matrizes que quer multiplicar e retorna um ponteiro da matriz resultante.
-//ex: mat_P = produtoMatriz(mat1, mat2);
 
-#endif // MATRIZ_H_INCLUDED
+
+#endif 
